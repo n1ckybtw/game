3e38f55bad763194240a614184ca9542b4dfdd8e
n1ckybtw <danilovnikita365@gmail.com>
2024-04-18 14:56:38 +0400

    d

--- app.py
+++ app.py
@@ -19,73 +19,73 @@ class Article(db.Model):
         return  '<Article %r>' % self.id
 
 
-@app.route('/')
-@app.route('/home')
+@app.route('https://n1ckybtw.github.io/')
+@app.route('https://n1ckybtw.github.io/home')
 def index():
     return render_template('index.html')
 
 
-@app.route("/background")
+@app.route("https://n1ckybtw.github.io/background")
 def background():
     return render_template('background.html')
 
 
-@app.route('/final')
+@app.route('https://n1ckybtw.github.io/final')
 def final():
     return render_template('final.html')
 
 
-@app.route('/defeat')
+@app.route('https://n1ckybtw.github.io/defeat')
 def defeat():
     return render_template('defeat.html')
 
 
-@app.route('/1')
+@app.route('https://n1ckybtw.github.io/1')
 def first():
     return render_template('task1.html')
 
 
-@app.route('/3')
+@app.route('https://n1ckybtw.github.io/3')
 def third():
     return render_template('task3.html')
 
 
-@app.route('/4')
+@app.route('https://n1ckybtw.github.io/4')
 def forth():
     return render_template('task4.html')
 
 
-@app.route('/5')
+@app.route('https://n1ckybtw.github.io/5')
 def firth():
     return render_template('task5.html')
 
 
-@app.route('/2')
+@app.route('https://n1ckybtw.github.io/2')
 def second():
     return render_template('task2.html')
 
 
-@app.route('/tasks')
+@app.route('https://n1ckybtw.github.io/tasks')
 def task():
     return render_template('task.html')
 
 
-@app.route('/start')
+@app.route('https://n1ckybtw.github.io/start')
 def start():
     return render_template('startgame.html')
 
-@app.route('/news')
+@app.route('https://n1ckybtw.github.io/news')
 def news():
     articles = Article.query.order_by(Article.date).all()
     return render_template('news.html', articles=articles)
 
 
-@app.route('/info')
+@app.route('https://n1ckybtw.github.io/info')
 def info():
     return render_template('info.html')
 
 
-@app.route('/create', methods=['POST', 'GET'])
+@app.route('https://n1ckybtw.github.io/create', methods=['POST', 'GET'])
 def create():
     if request == 'POST':
         title = request.form['title']
@@ -97,7 +97,7 @@ def create():
         try:
             db.session.add(article)
             db.session.commit()
-            return redirect('/news')
+            return redirect('https://n1ckybtw.github.io/news')
         except:
             return 'При добавлении произошла ошибка'
     else:
